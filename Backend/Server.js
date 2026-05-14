02bd8b1588053b36c53bcf81fa085dfa8532c560
mehra91 <pm7300779625@gmail.com>
2026-05-14 08:35:15 +0530

    calling DB

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = 4000;
@@ -9,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+//DB
+connectDB();
 
 //Routes
 app.get('/',(req,res)=>{
