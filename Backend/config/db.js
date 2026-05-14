341d05a648eb44105a6b9d56ab1627895351b6b3
mehra91 <pm7300779625@gmail.com>
2026-05-14 08:34:05 +0530

    connecting DB

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,1 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://pm7300779625_db_user:GcHeNS7QNyVvbtAF@cluster0.cbfimtw.mongodb.net/Expense",
+    )
+    .then(() => console.log("mongodb connected"));
+};
