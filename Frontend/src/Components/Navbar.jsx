f87f3386afea5f2fff4352fc438c83725e19bfa9
mehra91 <pm7300779625@gmail.com>
2026-05-19 13:20:29 +0530

    add lucide icons + adding navbar and working dropdown menu

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { StoreContext } from "../context/storeContext.jsx";
 import { navbarStyles } from '../assets/dummyStyles'
 import img1 from '../assets/logo.png';
 import { Navigate } from 'react-router-dom';
+import { ChevronDown, LogOut ,User } from 'lucide-react';
 
-const Navbar = () => {
-  const { logOut, user } = useContext(StoreContext);
+const Navbar = (prop) => {
+  const { logOut } = useContext(StoreContext);
+
+  const menuRef = useRef();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const user = prop.User || {
+    name: "",
+    email: ""
+  }
+
+  const toogleMenu = () => setMenuOpen((prev) => !prev)
 
   return (
     <header className="sticky top-0 z-50 bg-white border-b border-gray-100 shadow-sm">
@@ -21,6 +32,76 @@ const Navbar = () => {
           </span>
         </div>
         {/* if the user is present */}
+        {user && (
+          <div className='relative' ref={menuRef}>
+            <button onClick={toogleMenu} className='flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-50 transition-colors'>
+              <div className='relative '>
+                <div className='w-9 h-9 flex items-center justify-center  rounded-full bg-linear-to-br from-teal-600 to-cyan-500 text-white font-bold text-lg'>
+                  {user?.name?.[0]?.toUpperCase() || 'U'}
+                </div>
+                {/* green dot */}
+                <div className='absolute -bottom-1 -right-1 w-3 h-3 bg-green-500 rounded-full border-2 sborder-white'>
+                </div>
+
+              </div>
+              <div className='text-left  hidden md:block'>
+                <p className='text-sm font-medium text-gray-800 truncate max-w-30'>
+                  {user?.name || 'User'}
+                </p>
+                <p className='text-xs text-gray-500 truncate max-w-30'>
+                  {user?.email || 'user@gmail.com'}
+                </p>
+              </div>
+              <ChevronDown className={`${menuOpen}`} />
+            </button>
+            {/* dropdown Menu */}
+            {
+              menuOpen && (
+                <div className='absolute top-14 right-0 w-56 bg-white rounded-xl shadow-lg border border-gray-100 z-50'>
+                  <div className='px-4 py-3 border-b border-gray-100'>
+                    <div className='flex items-center gap-3'>
+                      <div className='w-10 h-10 rounded-full bg-linear-to-br from-teal-600 to-cyan-500 flex items-center justify-center text-white font-bold text-lg'>
+                        {user?.name?.[0]?.toUpperCase() || 'U'}
+
+                      </div>
+                      <div className='text-sm text-gray-800'>
+                        {user?.name || 'User'}
+                      </div>
+                      <div className='text-xs text-gray-500'>
+                        {user?.email || 'user@gmail.com'}
+                      </div>
+
+                    </div>
+                    <div className='p-1.5'>
+                      <button onClick={() => {
+                        setMenuOpen(false);
+                        Navigate('/profile')
+                      }} className='w-full px-4 py-3 text-left hover:bg-gray-50 text-sm text-gray-700 flex items-center gap-3 rounded-lg'>
+
+                        <User className='w-4 h-4' />
+                        <span>
+                          My profile
+                        </span>
+
+
+                      </button>
+                    </div>
+                    <div className='p-1.5 border-t border-gray-100'>
+                          <button onClick={logOut} className='flex w-full items-center gap-3 px-4 py-3 text-sm hover:bg-red-50 text-red-600 rounded-lg'>
+                              <LogOut className='w-4 h-4'/>
+                              <span>
+                                Log Out
+                              </span>
+                          </button>
+                    </div>
+                  </div>
+
+                </div>
+              )
+            }
+          </div>
+        )}
+
       </div>
     </header>
   )
